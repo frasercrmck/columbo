0388d621575d13abe301d8acbdd737c0d77908a5
Fraser Cormack <frasercrmck@gmail.com>
2016-01-08 13:44:46 +0000

    Organize init.h a bit; add a new puzzle

--- init.h
+++ init.h
@@ -7,8 +7,12 @@
 #include <map>
 #include <set>
 
+#define EASY_1 1
+#define EASY_2 0
+#define FIENDISH_1 0
+
 static void init(Grid *grid, CageList &cages) {
-#if 1
+#if EASY_1
   cages.push_back(Cage{26, grid, {{A, 0}, {B, 0}, {B, 1}, {C, 0}}});
   cages.push_back(Cage{12, grid, {{A, 1}, {A, 2}}});
   cages.push_back(Cage{7, grid, {{B, 2}, {C, 1}, {C, 2}}});
@@ -43,8 +47,7 @@ static void init(Grid *grid, CageList &cages) {
   cages.push_back(Cage{3, grid, {{H, 7}, {H, 8}}});
   cages.push_back(Cage{12, grid, {{J, 7}, {J, 8}}});
   cages.push_back(Cage{16, grid, {{G, 6}, {G, 7}}});
-#else
-#if 1
+#elif FIENDISH_1
   cages.push_back(Cage{22, grid, {{A, 0}, {A, 1}, {A, 2}}});
   cages.push_back(Cage{10, grid, {{A, 3}, {A, 4}, {B, 2}, {B, 3}}});
   cages.push_back(Cage{4, grid, {{A, 5}, {A, 6}}});
@@ -72,7 +75,7 @@ static void init(Grid *grid, CageList &cages) {
   cages.push_back(Cage{15, grid, {{H, 4}, {J, 4}, {J, 5}}});
   cages.push_back(Cage{5, grid, {{H, 5}, {H, 6}}});
   cages.push_back(Cage{16, grid, {{J, 6}, {J, 7}, {J, 8}}});
-#else
+#elif EASY_2
   cages.push_back(Cage{17, grid, {{A, 0}, {B, 0}, {C, 0}, {D, 0}}});
   cages.push_back(Cage{15, grid, {{A, 1}, {B, 1}, {C, 1}}});
   cages.push_back(Cage{10, grid, {{A, 2}, {B, 2}}});
@@ -104,7 +107,36 @@ static void init(Grid *grid, CageList &cages) {
   cages.push_back(Cage{18, grid, {{H, 4}, {H, 5}, {J, 4}, {J, 5}}});
   cages.push_back(Cage{14, grid, {{H, 6}, {J, 6}}});
   cages.push_back(Cage{8,  grid, {{J, 0}, {J, 1}, {J, 2}}});
-#endif
+#else
+  cages.push_back(Cage{3, grid, {{A, 0}, {A, 1}}});
+  cages.push_back(Cage{15, grid, {{A, 2}, {A, 3}, {A, 4}}});
+  cages.push_back(Cage{22, grid, {{A, 5}, {B, 5}, {B, 4}, {C, 4}}});
+  cages.push_back(Cage{4, grid, {{A, 6}, {B, 6}}});
+  cages.push_back(Cage{16, grid, {{A, 7}, {B, 7}}});
+  cages.push_back(Cage{15, grid, {{A, 8}, {B, 8}, {C, 8}, {D, 8}}});
+  cages.push_back(Cage{25, grid, {{B, 0}, {B, 1}, {C, 0}, {C, 1}}});
+  cages.push_back(Cage{17, grid, {{B, 2}, {B, 3}}});
+  cages.push_back(Cage{9, grid, {{C, 2}, {C, 3}, {D, 3}}});
+  cages.push_back(Cage{8, grid, {{C, 5}, {D, 5}, {E, 5}}});
+  cages.push_back(Cage{20, grid, {{C, 6}, {C, 7}, {D, 6}}});
+  cages.push_back(Cage{6, grid, {{D, 0}, {E, 0}}});
+  cages.push_back(Cage{14, grid, {{D, 1}, {D, 2}}});
+  cages.push_back(Cage{17, grid, {{D, 4}, {E, 4}, {F, 4}}});
+  cages.push_back(Cage{17, grid, {{D, 7}, {E, 7}, {E, 6}}});
+  cages.push_back(Cage{13, grid, {{E, 1}, {E, 2}, {F, 1}}});
+  cages.push_back(Cage{20, grid, {{E, 3}, {F, 3}, {G, 3}}});
+  cages.push_back(Cage{12, grid, {{E, 8}, {F, 8}}});
+  cages.push_back(Cage{27, grid, {{F, 0}, {G, 0}, {H, 0}, {J, 0}}});
+  cages.push_back(Cage{6, grid, {{F, 2}, {G, 2}, {G, 1}}});
+  cages.push_back(Cage{20, grid, {{F, 5}, {G, 5}, {G, 6}}});
+  cages.push_back(Cage{6, grid, {{F, 6}, {F, 7}}});
+  cages.push_back(Cage{10, grid, {{G, 4}, {H, 4}, {H, 3}, {J, 3}}});
+  cages.push_back(Cage{14, grid, {{G, 7}, {G, 8}, {H, 7}, {H, 8}}});
+  cages.push_back(Cage{8, grid, {{H, 1}, {J, 1}}});
+  cages.push_back(Cage{16, grid, {{H, 2}, {J, 2}}});
+  cages.push_back(Cage{15, grid, {{H, 5}, {H, 6}}});
+  cages.push_back(Cage{13, grid, {{J, 4}, {J, 5}, {J, 6}}});
+  cages.push_back(Cage{17, grid, {{J, 7}, {J, 8}}});
 #endif
 
   for (auto &cage : cages) {
