096dcb0f998a733d8687674c532314d758046885
Fraser Cormack <frasercrmck@gmail.com>
2016-01-08 16:21:33 +0000

    Add a helper to print a cell mask

--- debug.h
+++ debug.h
@@ -5,6 +5,9 @@
 #include <sstream>
 #include <iostream>
 
+#include "utils.h"
+#include "defs.h"
+
 static bool DEBUG = true;
 
 static std::string printCandidateString(unsigned long mask) {
@@ -19,6 +22,22 @@ static std::string printCandidateString(unsigned long mask) {
   return s.substr(0, s.size() - 1);
 }
 
-std::ostream & dbgs() { return std::cout; }
+std::ostream &dbgs() { return std::cout; }
+
+void printCellMask(House &house, const unsigned long mask) {
+  const int bit_count = bitCount(mask);
+  int found = 0;
+  for (unsigned i = 0; i < 9; ++i) {
+    const bool is_on = (mask >> i) & 0x1;
+    if (!is_on) {
+      continue;
+    }
+    ++found;
+    dbgs() << house[i]->coord;
+    if (found < bit_count) {
+      dbgs() << "/";
+    }
+  }
+}
 
 #endif // SOLVER_DEBUG_H
