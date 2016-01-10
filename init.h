f6c117c77ce4491e6084027a9934e83c25848822
Fraser Cormack <frasercrmck@gmail.com>
2016-01-10 15:21:09 +0000

    Add another sudoku example

--- init.h
+++ init.h
@@ -12,8 +12,9 @@
 #define EASY_1 0
 #define EASY_2 0
 #define FIENDISH_1 0
+#define DEADLY_1 1
 #define DIABOLICAL_1 0
-#define EXTREME_1 1
+#define EXTREME_1 0
 
 static bool initializeCages(Grid *grid) {
   CageList &cages = grid->cages;
@@ -80,6 +81,29 @@ static bool initializeCages(Grid *grid) {
   cages.push_back(Cage{15, grid, {{H, 4}, {J, 4}, {J, 5}}});
   cages.push_back(Cage{5, grid, {{H, 5}, {H, 6}}});
   cages.push_back(Cage{16, grid, {{J, 6}, {J, 7}, {J, 8}}});
+#elif DEADLY_1
+  cages.push_back(
+      Cage{28, grid, {{A, 0}, {A, 1}, {B, 0}, {B, 1}, {C, 0}, {C, 1}, {D, 1}}});
+  cages.push_back(Cage{24, grid, {{A, 2}, {B, 2}, {C, 2}, {D, 2}}});
+  cages.push_back(Cage{18, grid, {{A, 3}, {B, 3}, {B, 4}, {B, 5}}});
+  cages.push_back(Cage{26, grid, {{A, 4}, {A, 5}, {A, 6}, {A, 7}}});
+  cages.push_back(Cage{32, grid, {{A, 8}, {B, 8}, {C, 8}, {C, 7}, {C, 6}}});
+  cages.push_back(Cage{3, grid, {{B, 6}, {B, 7}}});
+  cages.push_back(Cage{20, grid, {{C, 3}, {C, 4}, {C, 5}, {D, 3}}});
+  cages.push_back(Cage{20, grid, {{D, 0}, {E, 0}, {E, 1}, {E, 2}}});
+  cages.push_back(Cage{13, grid, {{D, 4}, {D, 5}, {E, 4}, {E, 5}}});
+  cages.push_back(Cage{30, grid, {{D, 6}, {D, 7}, {E, 6}, {E, 7}}});
+  cages.push_back(Cage{13, grid, {{D, 8}, {E, 8}, {F, 8}, {G, 8}}});
+  cages.push_back(Cage{12, grid, {{E, 3}, {F, 3}, {G, 3}, {G, 2}}});
+  cages.push_back(Cage{24, grid, {{F, 0}, {G, 0}, {H, 0}, {J, 0}}});
+  cages.push_back(Cage{9, grid, {{F, 1}, {F, 2}}});
+  cages.push_back(Cage{23, grid, {{F, 4}, {F, 5}, {G, 4}}});
+  cages.push_back(Cage{25, grid, {{F, 6}, {F, 7}, {G, 7}, {H, 7}}});
+  cages.push_back(Cage{29, grid, {{G, 1}, {H, 1}, {H, 2}, {J, 1}, {J, 2}}});
+  cages.push_back(Cage{9, grid, {{G, 5}, {G, 6}}});
+  cages.push_back(Cage{21, grid, {{H, 3}, {H, 4}, {J, 3}, {J, 4}}});
+  cages.push_back(Cage{14, grid, {{H, 5}, {H, 6}, {J, 5}, {J, 6}}});
+  cages.push_back(Cage{12, grid, {{H, 8}, {J, 8}, {J, 7}}});
 #elif DIABOLICAL_1
   cages.push_back(Cage{32, grid, {{A, 0}, {A, 1}, {A, 2}, {B, 1}, {B, 2}}});
   cages.push_back(Cage{19, grid, {{A, 3}, {A, 4}, {A, 5}, {A, 6}}});
